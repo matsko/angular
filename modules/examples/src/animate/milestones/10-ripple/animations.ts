a0cf84717d67db72abfdf7e32f5fc0ab485ef8f0
Robert Messerle <rmesserle@google.com>
2015-10-20 05:16:30 +0100

    fixes ripple to use jQuery + jQuery.transit

--- modules/examples/src/animate/milestones/10-ripple/animations.ts
+++ modules/examples/src/animate/milestones/10-ripple/animations.ts
@@ -1,18 +1,8 @@
 import {AnimationFactory} from 'angular2/src/animate/animate';
-import {
-  waitDelay,
-  query,
-  staggerTimers,
-  keyframe,
-  stagger,
-  style,
-  transition,
-  group,
-  chain,
-  parallel,
-  RAFRunner,
-  AnimationEventContext
-} from 'angular2/src/animate/animate';
+import { waitDelay, query, staggerTimers, keyframe, stagger, style, transition, group, chain,
+  parallel, RAFRunner, AnimationEventContext } from 'angular2/src/animate/animate';
+
+declare var jQuery;
 
 var animations = new AnimationFactory();
 
@@ -24,20 +14,16 @@ animations.add('animate-app', (ctx) => {
 
   function animateRipple (element, context) {
     var event = context.detail.collectedEvents[ 0 ];
-    var ripple = createRipple(element);
 
-    return chain([
-      style(getInitialStyles(element, event)),
-      transition({ opacity: 0.5, transform: 'translate(-50%,-50%)' }, 650),
-      transition({ opacity: 0, left: '50%', top: '50%' }, 650)
-    ]).start(ripple, context);
-  }
+    var elem = jQuery('<div class="ripple test">')
+      .appendTo(element)
+      .css(getInitialStyles(element, event))
+      .transit({ left: '50%', top: '50%', opacity: 0.5, transform: 'translate(-50%, -50%) scale(1)' }, 450)
+      .transit({ opacity: 0 }, 450, remove);
 
-  function createRipple(parent) {
-    var ripple = document.createElement('div');
-    ripple.className = 'ripple';
-    parent.appendChild(ripple);
-    return ripple;
+    function remove () {
+      elem.remove();
+    }
   }
 
   function getInitialStyles(element, event) {
@@ -54,6 +40,6 @@ animations.add('animate-app', (ctx) => {
     var x = element.offsetWidth;
     var y = element.offsetHeight;
     var size = Math.sqrt(x * x + y + y);
-    return Math.ceil(size);
+    return Math.ceil(size * 1.3);
   }
 });
--- modules/examples/src/animate/milestones/11-tabs/animations.ts
+++ modules/examples/src/animate/milestones/11-tabs/animations.ts
@@ -1,26 +1,22 @@
 import {AnimationFactory} from 'angular2/src/animate/animate';
-import {
-  waitDelay,
-  query,
-  staggerTimers,
-  keyframe,
-  stagger,
-  style,
-  transition,
-  group,
-  chain,
-  parallel,
-  RAFRunner,
-  AnimationEventContext
-} from 'angular2/src/animate/animate';
+import { waitDelay, query, staggerTimers, keyframe, stagger, style, transition, group, chain,
+  parallel, RAFRunner, AnimationEventContext } from 'angular2/src/animate/animate';
 
 var animations = new AnimationFactory();
 
 animations.add('animate-app', (ctx) => {
   var loaded = false;
-  ctx.onAttrChange('data-index', (element, value, ctx) => {
+
+  ctx.onAttrChange('data-index', (element, value) => {
+    positionContent(element.getElementsByClassName('tabs-content')[0], value);
+    positionInkBar(element.getElementsByTagName('ink-bar')[0], value);
+
+    loaded = true;
+  });
+
+  function positionInkBar(element, value) {
     var selectedLabel = Array.prototype.slice.apply(element.parentElement.children)
-        .filter(element => element.matches('a'))[ value ];
+      .filter(element => element.matches('a'))[ value ];
     var left = selectedLabel.offsetLeft;
     var right = element.parentElement.clientWidth - left - selectedLabel.offsetWidth;
 
@@ -32,7 +28,35 @@ animations.add('animate-app', (ctx) => {
     //-- set new position
     element.style.left = left + 'px';
     element.style.right = right + 'px';
+  }
 
-    loaded = true;
-  }, element => element.matches('ink-bar'));
+  function positionContent(element, value) {
+    Array.prototype.slice.apply(element.children).forEach((element, index) => {
+
+      //-- on initial load, skip the transition
+      if (!loaded) element.classList.add('no-transition');
+      else element.classList.remove('no-transition');
+
+      //-- toggle classes appropriately for left/right movement
+      if (index < value) toggleClasses('right', 'left');
+      else if (index > value) toggleClasses('left', 'right');
+      else removeAll();
+
+      //-- after initial positioning, remove `no-transition` class
+      if (!loaded) element.classList.remove('no-transition');
+
+      function removeAll() {
+        element.classList.remove('right');
+        element.classList.remove('left');
+      }
+
+      function toggleClasses(a, b) {
+        if (element.classList.contains(a)) {
+          element.classList.add('no-transition');
+          element.classList.remove(a);
+        }
+        element.classList.add(b);
+      }
+    });
+  }
 });
